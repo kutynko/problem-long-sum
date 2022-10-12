bd78fb9d79970d839f66ce2eac4506f81c5bebb4
Ruslan Kutynko <kutynko@hotmail.com>
2022-10-12 21:42:31 +0300

    решение

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ function calculate(e) {
 	e.preventDefault();
 	const formData = new FormData(e.target);
 
-	const a = formData.get('a');
-	const b = formData.get('b');
+	let a = formData.get('a');
+	let b = formData.get('b');
 
-	const aNumber = parseInt(a);
-	const bNumber = parseInt(b);
+	let filler = '';
+	for (let i = 0; i < Math.abs(a.length - b.length); i++) filler += '0'
+	if (a.length < b.length) a = filler + a;
+	else b = filler + b;
 
-	result.innerHTML = aNumber + bNumber;
+	let digit = 0;
+	let res = '';
+	for (let i = a.length - 1; i >=0; i--) {
+		const ai = parseInt( a[i])
+		const bi = parseInt( b[i])
+		const ri = ai + bi + digit;
+		res = ri % 10 + res;
+		digit = Math.floor(ri / 10);
+	}
+
+	result.innerHTML = res;
 }
